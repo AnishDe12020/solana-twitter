f5c5062651780c47fd107fda1edbfdab937111b9
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-05-10 21:41:09 +0530

    fix(send-tweet-instruction): fix error handling and the system program field

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -7,17 +7,17 @@ declare_id!("A6veVg75tBf31GQMXqdkJWh2goVs99gN7DruUB1Zmsb1");
 pub mod solana_twitter {
     use super::*;
 
-    pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> ProgramResult {
+    pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> Result<()> {
         let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet;
         let author: &Signer = &ctx.accounts.author;
         let clock: Clock = Clock::get().unwrap();
 
         if topic.chars().count() > 50 {
-            return Err(ErrorCode::TopicTooLong.into());
+            return Err(error!(ErrorCode::TopicTooLong));
         }
 
         if content.chars().count() > 280 {
-            return Err(ErrorCode::ContentTooLong.into());
+            return Err(error!(ErrorCode::ContentTooLong));
         }
 
         tweet.author = *author.key;
@@ -36,14 +36,8 @@ pub struct SendTweet<'info> {
     #[account(mut)]
     pub author: Signer<'info>,
     #[account(address = system_program::ID)]
-    pub system_program: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
 }
-// OR
-// #[derive(Accounts)]
-// pub struct SendTweet<'info> {
-//   // ...
-// pub system_program: Program<'info, System>,
-// }
 
 // 1. Define the structure of the Tweet account.
 #[account]
