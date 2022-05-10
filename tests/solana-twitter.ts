8c505921248d226bff67a44839fab0773b5cd5ea
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-05-10 21:50:21 +0530

    feat(test): add another test to see if other users can send tweets

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -62,4 +62,44 @@ describe("solana-twitter", () => {
     assert.equal(tweetAccount.content, "gm wagmi");
     assert.ok(tweetAccount.timestamp);
   });
+
+  it("can send a new tweet from a different author", async () => {
+    // Generate another user and airdrop them some SOL.
+    const otherUser = anchor.web3.Keypair.generate();
+    const signature = await program.provider.connection.requestAirdrop(
+      otherUser.publicKey,
+      1000000000
+    );
+    await program.provider.connection.confirmTransaction(signature);
+
+    // Call the "SendTweet" instruction on behalf of this other user.
+    const tweet = anchor.web3.Keypair.generate();
+    await program.rpc.sendTweet(
+      "apple",
+      "MACS ARE GETTING USER UPGRADABLE RAM AND STORAGE!!!",
+      {
+        accounts: {
+          tweet: tweet.publicKey,
+          author: otherUser.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [otherUser, tweet],
+      }
+    );
+
+    // Fetch the account details of the created tweet.
+    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+
+    // Ensure it has the right data.
+    assert.equal(
+      tweetAccount.author.toBase58(),
+      otherUser.publicKey.toBase58()
+    );
+    assert.equal(tweetAccount.topic, "apple");
+    assert.equal(
+      tweetAccount.content,
+      "MACS ARE GETTING USER UPGRADABLE RAM AND STORAGE!!!"
+    );
+    assert.ok(tweetAccount.timestamp);
+  });
 });
