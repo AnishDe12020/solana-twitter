9bb4db41925a14cf7f4aa288bddae17314bed1c1
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-05-10 20:02:15 +0530

    feat: write a instruction to send tweet

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::system_program;
 
 declare_id!("A6veVg75tBf31GQMXqdkJWh2goVs99gN7DruUB1Zmsb1");
 
@@ -6,13 +7,43 @@ declare_id!("A6veVg75tBf31GQMXqdkJWh2goVs99gN7DruUB1Zmsb1");
 pub mod solana_twitter {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> ProgramResult {
+        let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet;
+        let author: &Signer = &ctx.accounts.author;
+        let clock: Clock = Clock::get().unwrap();
+
+        if topic.chars().count() > 50 {
+            return Err(ErrorCode::TopicTooLong.into());
+        }
+
+        if content.chars().count() > 280 {
+            return Err(ErrorCode::ContentTooLong.into());
+        }
+
+        tweet.author = *author.key;
+        tweet.timestamp = clock.unix_timestamp;
+        tweet.topic = topic;
+        tweet.content = content;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct SendTweet<'info> {
+    #[account(init, payer = author, space = Tweet::LEN)]
+    pub tweet: Account<'info, Tweet>,
+    #[account(mut)]
+    pub author: Signer<'info>,
+    #[account(address = system_program::ID)]
+    pub system_program: AccountInfo<'info>,
+}
+// OR
+// #[derive(Accounts)]
+// pub struct SendTweet<'info> {
+//   // ...
+// pub system_program: Program<'info, System>,
+// }
 
 // 1. Define the structure of the Tweet account.
 #[account]
@@ -38,4 +69,12 @@ impl Tweet {
         + TIMESTAMP_LENGTH // Timestamp.
         + STRING_LENGTH_PREFIX + MAX_TOPIC_LENGTH // Topic.
         + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH; // Content.
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The provided topic should be 50 characters long maximum.")]
+    TopicTooLong,
+    #[msg("The provided content should be 280 characters long maximum.")]
+    ContentTooLong,
 }
