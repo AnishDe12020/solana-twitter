d3386d03236079080935d55d5d436e0e644bd72d
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-05-11 09:38:48 +0530

    test: added test to fetch all tweets, by auth public key and by topic

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -2,6 +2,7 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { SolanaTwitter } from "../target/types/solana_twitter";
 import * as assert from "assert";
+import * as bs58 from "bs58";
 
 describe("solana-twitter", () => {
   // Configure the client to use the local cluster.
@@ -147,5 +148,53 @@ describe("solana-twitter", () => {
     }
 
     assert.fail("The instruction should fail with a 281-character content.");
+  });
+
+  it("can fetch all tweets", async () => {
+    const tweetAccounts = await program.account.tweet.all();
+    assert.equal(tweetAccounts.length, 3);
+  });
+
+  it("can filter tweets by author", async () => {
+    const authorPublicKey = program.provider.wallet.publicKey;
+    const tweetAccounts = await program.account.tweet.all([
+      {
+        memcmp: {
+          offset: 8, // Discriminator.
+          bytes: authorPublicKey.toBase58(),
+        },
+      },
+    ]);
+
+    assert.equal(tweetAccounts.length, 2);
+    assert.ok(
+      tweetAccounts.every((tweetAccount) => {
+        return (
+          tweetAccount.account.author.toBase58() === authorPublicKey.toBase58()
+        );
+      })
+    );
+  });
+
+  it("can filter tweets by topics", async () => {
+    const tweetAccounts = await program.account.tweet.all([
+      {
+        memcmp: {
+          offset:
+            8 + // Discriminator.
+            32 + // Author public key.
+            8 + // Timestamp.
+            4, // Topic string prefix.
+          bytes: bs58.encode(Buffer.from("javascript")),
+        },
+      },
+    ]);
+
+    assert.equal(tweetAccounts.length, 1);
+    assert.ok(
+      tweetAccounts.every((tweetAccount) => {
+        return tweetAccount.account.topic === "javascript";
+      })
+    );
   });
 });
