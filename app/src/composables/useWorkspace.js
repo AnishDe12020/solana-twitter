f0d44486f6d55d17868d0d8387869b065b46ba59
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-05-11 14:02:30 +0530

    chore: added devnet/mainnet deploy stuff

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -2,18 +2,19 @@ import { useAnchorWallet } from "solana-wallets-vue";
 import { Connection, PublicKey } from "@solana/web3.js";
 import { computed } from "vue";
 import { Program, AnchorProvider } from "@project-serum/anchor";
-import idl from "../../../target/idl/solana_twitter.json";
+import idl from "@/idl/solana_twitter.json";
 
+const clusterUrl = process.env.VUE_APP_CLUSTER_URL;
 const preflightCommitment = "processed";
 const commitment = "processed";
 const programID = new PublicKey(idl.metadata.address);
 let workspace = null;
 
 export const useWorkspace = () => workspace;
-
+("http://127.0.0.1:8899");
 export const initWorkspace = () => {
   const wallet = useAnchorWallet();
-  const connection = new Connection("http://127.0.0.1:8899", commitment);
+  const connection = new Connection(clusterUrl, commitment);
   const provider = computed(
     () =>
       new AnchorProvider(connection, wallet.value, {
