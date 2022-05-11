9b0a985db2fae431a13b726d4dea9eb9194e7211
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-05-11 14:56:41 +0530

    style(app,tweet-form): make connect your wallet message dark mode

--- app/src/components/TweetForm.vue
+++ app/src/components/TweetForm.vue
@@ -104,7 +104,7 @@ const send = async () => {
 
   <div
     v-else
-    class="px-8 py-4 bg-gray-50 text-gray-500 text-center border-b border-tertiary"
+    class="px-8 py-4 text-gray-500 text-center border-b border-tertiary bg-secondary"
   >
     Connect your wallet to start tweeting...
   </div>
