2c14808445bd5072d4dfaafa4339d5ea0b8758db
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-05-11 13:18:27 +0530

    feat(app): added ability to send tweets

--- app/src/api/send-tweet.js
+++ app/src/api/send-tweet.js
@@ -1,10 +1,27 @@
+import { web3 } from "@project-serum/anchor";
+import { useWorkspace } from "@/composables";
+import { Tweet } from "@/models";
+
+// 1. Define the sendTweet endpoint.
 export const sendTweet = async (topic, content) => {
-    return {
-        topic,
-        content,
-        author_display: 'B1Af..wtRN',
-        created_at: 'Nov 26, 2021 1:03PM',
-        created_ago: 'just now',
-        timestamp: 1637932868,
-    }
-}
+  const { wallet, program } = useWorkspace();
+
+  // 2. Generate a new Keypair for our new tweet account.
+  const tweet = web3.Keypair.generate();
+
+  // 3. Send a "SendTweet" instruction with the right data and the right accounts.
+  await program.value.rpc.sendTweet(topic, content, {
+    accounts: {
+      author: wallet.value.publicKey,
+      tweet: tweet.publicKey,
+      systemProgram: web3.SystemProgram.programId,
+    },
+    signers: [tweet],
+  });
+
+  // 4. Fetch the newly created account from the blockchain.
+  const tweetAccount = await program.value.account.tweet.fetch(tweet.publicKey);
+
+  // 5. Wrap the fetched account in a Tweet model so our frontend can display it.
+  return new Tweet(tweet.publicKey, tweetAccount);
+};
