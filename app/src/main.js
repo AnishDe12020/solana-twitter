6991d2e0cdb6e925a4b31a988ca2e30fd500e24b
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-05-11 11:58:48 +0530

    chore: init vue frontend, add tailwind and set up routes

---
+++ app/src/main.js
@@ -0,0 +1,13 @@
+import "./main.css";
+
+import { createRouter, createWebHashHistory } from "vue-router";
+import routes from "./routes";
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).use(router).mount("#app");
---
+++ app/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  purge: ["./public/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
